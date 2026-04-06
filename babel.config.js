9519e2532da65784ccb795e2543614676772705a
LY-road2026 <314790063@qq.com>
2026-04-06 16:13:43 +0800

    Add babel-plugin-module-resolver for @ path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
     'react-native-worklets-core/plugin',
     'react-native-reanimated/plugin',
   ],
